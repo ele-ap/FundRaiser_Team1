bf5f8f0a5a54afcf3c6153fe35fdc5db53bea411
Aikaterini Maria Gerakianaki <katerinagerak99@gmail.com>
2021-11-25 16:51:22 +0200

    BackerServices and Interface

---
+++ FundRaiser_Team1/Services/BackerService.cs
@@ -0,0 +1,36 @@
+﻿using FundRaiser_Team1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundRaiser_Team1.Services
+{
+    public class BackerService : IBackerService
+    {
+        private readonly FundRaiserDbContext _db;
+
+        public BackerService(FundRaiserDbContext adbContext)
+        {
+            _db = adbContext;
+        }
+        public void CreateBacker(Backer backer)
+        {
+            _db.Backer.Add(backer);
+            try { _db.SaveChanges(); }
+            catch { }
+        }
+
+        public Backer ReadBacker(int id)
+        {
+            Backer backer = _db.Backer.Find(id);
+            return backer;
+        }
+
+        public List<Backer> ReadBacker()
+        {
+            return _db.Backer.ToList();
+        }
+    }
+}
---
+++ FundRaiser_Team1/Services/IBackerService.cs
@@ -0,0 +1,17 @@
+﻿using FundRaiser_Team1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundRaiser_Team1.Services
+{
+    public interface IBackerService
+    {
+        public void CreateBacker(Backer backer);
+        public Backer ReadBacker(int id);
+        
+        public List<Backer> ReadBacker();
+    }
+}
