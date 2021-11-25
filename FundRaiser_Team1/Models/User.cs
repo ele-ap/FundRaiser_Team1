eecf5bf0e6be7885d212d28b8a39d429a63b3ae1
Aikaterini Maria Gerakianaki <katerinagerak99@gmail.com>
2021-11-25 11:34:08 +0200

    User's Class and Creator's Class

---
+++ FundRaiser_Team1/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundRaiser_Team1.Models
+{
+    public abstract class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public User(string FirstName,string LastName)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+        }
+    }
+}
