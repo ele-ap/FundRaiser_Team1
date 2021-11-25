c84fc8ff7f46a9326bc34fcad2f66744e4ddd53a
Apostolos Eleftheriou (Απόστολος Ελευθερίου) <apostolos.eleftheriou@citrix.com>
2021-11-25 11:06:50 +0200

    Define Project class

---
+++ FundRaiser_Team1/Model/Project.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundRaiser_Team1.Model
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string StatusPost { get; set; }
+        public List<string> Photos { get; set; }
+        public List<string> Videos { get; set; }
+    }
+}
