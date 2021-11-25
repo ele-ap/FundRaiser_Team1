b8666e4e678df793e669f14dd0616c970aac8e17
Apostolos Eleftheriou (Απόστολος Ελευθερίου) <apostolos.eleftheriou@citrix.com>
2021-11-25 10:24:51 +0200

    Add project files.

---
+++ FundRaiser_Team1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FundRaiser_Team1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
