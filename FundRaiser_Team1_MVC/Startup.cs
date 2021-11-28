0880b26542928124954d42c175d90dcdae2f2fd4
Apostolos Eleftheriou <eleftheriouapp@gmail.com>
2021-11-28 12:23:19 +0200

    Added project controller.

--- FundRaiser_Team1_MVC/Startup.cs
+++ FundRaiser_Team1_MVC/Startup.cs
@@ -1,3 +1,4 @@
+using FundRaiser_Team1.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -24,6 +25,7 @@ namespace FundRaiser_Team1_MVC
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddScoped<IProjectService, ProjectService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
