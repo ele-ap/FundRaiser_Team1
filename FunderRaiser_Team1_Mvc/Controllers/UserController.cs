a34f4973d453d18f4d0973731e03b331551c6441
Aikaterini Maria Gerakianaki <katerinagerak99@gmail.com>
2021-11-28 11:22:19 +0200

    CreateUser correction

---
+++ FunderRaiser_Team1_Mvc/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace FunderRaiser_Team1_Mvc.Controllers
+{
+    public class UserController : Controller
+    {
+        //private IUserService userService;
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult CreateUser()
+        {
+            return View();
+        }
+    }
+}
