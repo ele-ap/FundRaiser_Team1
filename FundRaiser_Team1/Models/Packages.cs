54f69165aa36539575fe326c0714aab803808a96
Vasileios Chatzis <vchatzis@athtech.gr>
2021-11-29 17:26:23 +0200

    PackagesClass

---
+++ FundRaiser_Team1/Models/Packages.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Packages
+{
+	public int Id { get; set; }
+	public string PackageName { get; set; }
+	public string Description { get; set; }
+	public decimal PackagePrice { get; set; }
+	public List<Project> Projects { get; set; }
+
+}
---
+++ FundRaiser_Team1/Services/PackageService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundRaiser_Team1.Services
+{
+
+    public class PackageService : IPackageService
+    {
+        public void CreatePackage(Packages package)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Packages ReadPackage(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Packages> ReadPackages()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
