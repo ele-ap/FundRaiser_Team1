af837bb142399630552ac408a6e135b9e9b4447c
Aikaterini Maria Gerakianaki <katerinagerak99@gmail.com>
2021-12-03 18:28:34 +0200

    Correction User's Controller

--- FundRaiser_Team1_MVC/Controllers/HomeController.cs
+++ FundRaiser_Team1_MVC/Controllers/HomeController.cs
@@ -1,12 +1,12 @@
-﻿using FundRaiser_Team1.Models;
-using FundRaiser_Team1_MVC.Models;
+﻿using FundRaiser_Team1_MVC.Models;
+using FundRaiser_Team1.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Data;
+using System.Web;
 
 namespace FunderRaiser_Team1_Mvc.Controllers
 {
@@ -28,14 +28,66 @@ namespace FunderRaiser_Team1_Mvc.Controllers
         {
             return View();
         }
-      
+
         public IActionResult CreateUser()
         {
             return View();
         }
-    
+        public IActionResult LogOut()
+        {
+            //Delete Cookies
+            if (Request.Cookies["userId"] != null)
+            {
+                Response.Cookies.Delete("userId");
+            }
+            return RedirectToAction(nameof(SignIn));
+        }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        [HttpPost]
+        public IActionResult SignIn(String email, string password)
+        {
+
+            try
+            {
+                using (FundRaiserDbContext db = new FundRaiserDbContext())
+                {
+                    User u = (from user in db.User
+                              where user.Email == email
+                              select user).SingleOrDefault();
+
+                    if (u != null)
+                    {
+
+                        if (password.Equals(u.Password))
+                        {
+                            HttpContext.Response.Cookies.Append("userId", u.Id.ToString());
+
+                            if (((u.Category).ToString()).Equals("CREATOR"))
+                            {
+                                return RedirectToAction(nameof(Index));
+                            }
+                            if (((u.Category).ToString()).Equals("BACKER"))
+                            {
+                                return RedirectToAction(nameof(Index));
+                            }
+                            if (((u.Category).ToString()).Equals("BOTH"))
+                            {
+                                return RedirectToAction(nameof(Index));
+                            }
+
+                        }
+                        return View();
+                    }
+                }
+            }
+            catch
+            {
+                throw;
+            }
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
