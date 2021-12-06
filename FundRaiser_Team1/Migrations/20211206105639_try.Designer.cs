61c69d5ac4514ed6a2f36cf5ef44f39cc9f904f8
Thodoris Thomaidis <thomaidistheo@outlook.com>
2021-12-06 13:12:46 +0200

    js script to fill hidden input

--- FundRaiser_Team1/Migrations/20211206105639_try.Designer.cs
+++ FundRaiser_Team1/Migrations/20211206105639_try.Designer.cs
@@ -10,7 +10,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FundRaiser_Team1.Migrations
 {
     [DbContext(typeof(FundRaiserDbContext))]
-    [Migration("20211206101621_try")]
+    [Migration("20211206105639_try")]
     partial class @try
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
