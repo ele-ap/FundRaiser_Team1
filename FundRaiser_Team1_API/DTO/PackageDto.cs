e93078054dae3dcfa818f5b8c0c0b23b2d66d651
Apostolos Eleftheriou <eleftheriouapp@gmail.com>
2021-12-07 11:42:53 +0200

    Edited package dto.

--- FundRaiser_Team1_API/DTO/PackageDto.cs
+++ FundRaiser_Team1_API/DTO/PackageDto.cs
@@ -7,7 +7,6 @@ namespace FundRaiser_Team1_API.DTO
 {
     public class PackageDto
     {
-        public int Id { get; set; }
         public string PackageName { get; set; }
         public string Description { get; set; }
         public decimal PackagePrice { get; set; }
